d4817d7934c4db6d160d6e9e21aaed2547a5b939
Zuif <unconfigured@null.spigotmc.org>
2022-09-01 20:54:51 +0300

    heroku test2

--- src/main/java/me/zuif/hw2/config/FlywayConfig.java
+++ src/main/java/me/zuif/hw2/config/FlywayConfig.java
@@ -3,9 +3,9 @@ package me.zuif.hw2.config;
 import org.flywaydb.core.Flyway;
 
 public class FlywayConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "root";
+    private static final String URL = "ec2-34-235-198-25.compute-1.amazonaws.com:5432/ddd88hlsi18tn7";
+    private static final String USER = "ihgfwqtptjwyli";
+    private static final String PASSWORD = "a03f2586d8b5550d68fd62f29728add992e6c44e15e528a5a1b51bd24de03eea";
     private static final String SCHEMA = "hibernate_db";
     private static final String LOCATION = "db/migration";
 
