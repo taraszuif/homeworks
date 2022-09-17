5d750158e52f05ab1ca7d164c2b31fee144ecbc4
Zuif <unconfigured@null.spigotmc.org>
2022-09-17 18:47:10 +0300

    homework17

--- src/main/java/me/zuif/hw2/Main.java
+++ src/main/java/me/zuif/hw2/Main.java
@@ -27,10 +27,6 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        int hours = 60;
-        int days = (int) Math.floor(hours / 24);
-        int hours1 = hours - days * 24;
-        System.out.println("days " + days + " hours " + hours1);
         /* concurrencyTest();*/
         /* mongoTest();*/
         /* hibernateTest();*/
